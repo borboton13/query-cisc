be32ba062c2fe8da3c048762837800aed61f17ba
Ariel S <ariel.siles@ilvabolivia.com>
2019-07-02 16:27:08 -0400

    02.07.2019 1627

--- 2019/reporte_ventas.sql
+++ 2019/reporte_ventas.sql
@@ -9,7 +9,7 @@ from articulos_pedido a
 left join pedidos p on a.idpedidos = p.`IDPEDIDOS`
 left join personacliente pc on p.`IDCLIENTE` = pc.`IDPERSONACLIENTE`
 left join inv_articulos ar on a.`cod_art` = ar.`cod_art`
-where p.`FECHA_ENTREGA` between '2019-05-01' and '2019-05-31'
+where p.`FECHA_ENTREGA` between '2019-06-01' and '2019-06-30'
 and P.`ESTADO` <> 'ANULADO'
 and p.`IDUSUARIO` <> 5
 -- AND pc.`NOM` LIKE '%TORRES%'
@@ -25,7 +25,7 @@ from articulos_pedido a
 left join pedidos p on a.idpedidos = p.`IDPEDIDOS`
 left join personacliente pc on p.`IDCLIENTE` = pc.`IDPERSONACLIENTE`
 left join inv_articulos ar on a.`cod_art` = ar.`cod_art`
-where p.`FECHA_ENTREGA` between '2019-05-01' and '2019-05-31'
+where p.`FECHA_ENTREGA` between '2019-06-01' and '2019-06-30'
 and P.`ESTADO` <> 'ANULADO'
 and p.`IDUSUARIO` <> 5
 -- AND p.`IDCLIENTE` = 165
@@ -43,11 +43,10 @@ from articulos_pedido a
 left join pedidos p on a.idpedidos = p.`IDPEDIDOS`
 left join personacliente pc on p.`IDCLIENTE` = pc.`IDPERSONACLIENTE`
 left join inv_articulos ar on a.`cod_art` = ar.`cod_art`
-where p.`FECHA_ENTREGA` between '2019-05-21' and '2019-05-28'
+where p.`FECHA_ENTREGA` between '2019-06-01' and '2019-06-30'
 and P.`ESTADO` <> 'ANULADO'
 and p.`IDUSUARIO` <> 5
 and p.`IDTIPOPEDIDO` in (1, 5)
-and a.`cod_art` in (128, 129, 130, 131, 132)
 group by a.`cod_art`, ar.`descri`;
 
 -- --------------------------------------------------------------
@@ -59,7 +58,7 @@ from articulos_pedido a
 left join ventadirecta v on a.`IDVENTADIRECTA` = v.`IDVENTADIRECTA`
 left join personacliente pc on v.`IDCLIENTE` = pc.`IDPERSONACLIENTE`
 left join inv_articulos ar on a.`cod_art` = ar.`cod_art`
-where v.`FECHA_PEDIDO` between '2019-05-01' and '2019-05-31'
+where v.`FECHA_PEDIDO` between '2019-06-01' and '2019-06-30'
 and v.`ESTADO` <> 'ANULADO'
 -- AND ar.`cuenta_art` = '4420110201'
 and v.`IDUSUARIO` <> 5
@@ -76,7 +75,7 @@ from articulos_pedido a
 left join ventadirecta v on a.`IDVENTADIRECTA` = v.`IDVENTADIRECTA`
 left join personacliente pc on v.`IDCLIENTE` = pc.`IDPERSONACLIENTE`
 left join inv_articulos ar on a.`cod_art` = ar.`cod_art`
-where v.`FECHA_PEDIDO` between '2019-05-01' and '2019-05-31'
+where v.`FECHA_PEDIDO` between '2019-06-01' and '2019-06-30'
 and v.`ESTADO` <> 'ANULADO'
 -- AND V.`IDUSUARIO` IN (6, 404)
 and v.`IDUSUARIO` <> 5
