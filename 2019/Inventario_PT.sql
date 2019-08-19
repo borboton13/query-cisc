8a72203db294e8a6338d2ccb211584ed15c1643f
Ariel S <ariel.siles@ilvabolivia.com>
2019-08-19 16:35:57 -0400

    19.08.2019 1635

--- 2019/Inventario_PT.sql
+++ 2019/Inventario_PT.sql
@@ -1,5 +1,6 @@
 select 	t.`cod_art`, t.`nombre`, 
-	t.`cantidad` + IFNULL(pp.prod_total, 0) - IFNULL(vv.total, 0) + IFNULL(d.cantidad, 0) - IFNULL(ba.cantidad, 0) - IFNULL(br.cantidad, 0) + IFNULL(rec.cantidad, 0) - IFNULL(egr.cantidad, 0) as inv_final
+	t.`cantidad` + IFNULL(pp.prod_total, 0) - IFNULL(vv.total, 0) + IFNULL(d.cantidad, 0) - IFNULL(ba.cantidad, 0) - IFNULL(br.cantidad, 0) + IFNULL(rec.cantidad, 0) - IFNULL(egr.cantidad, 0) as inv_final,
+	a.estado
 from inv_inicio t -- -> Inventario inicial, cargar los nuevos productos 
 left join (
 	select v.`cod_art`, a.`descri`, SUM(v.`TOTAL`) as total
@@ -7,13 +8,13 @@ left join (
 	join inv_inventario i on v.`cod_art` = i.`cod_art`
 	join inv_articulos a  on i.`cod_art` = a.`cod_art`
 	where i.`cod_alm` = 2
-	and v.`FECHA` between '2019-01-01' and '2019-07-31'
+	and v.`FECHA` between '2019-01-01' and '2019-08-17'
 	group by v.`cod_art`, a.`descri`
 	) vv on t.`cod_art` = vv.cod_art
 left join (
 	select p.`COD_ART`, p.`NOMBRE`, SUM((p.`CANTIDAD_SC` + p.`REPROCESO_SC`)) as prod_total
 	from producciontotal p
-	where p.`FECHA` between '2019-01-01' and '2019-07-31'
+	where p.`FECHA` between '2019-01-01' and '2019-08-17'
 	group by p.`COD_ART`, p.`NOMBRE`
 	) pp on t.`cod_art` = pp.cod_art
 left join (
@@ -21,7 +22,7 @@ left join (
 	select d.`cod_art`, d.`descri`, SUM(d.`cantidad`) as cantidad
 	from inv_devolucion d
 	where d.`cod_alm` = 2
-	and d.`fecha` between '2019-01-01' and '2019-07-31'
+	and d.`fecha` between '2019-01-01' and '2019-08-17'
 	group by d.`cod_art`, d.`descri`
 	) d on t.cod_art = d.cod_art
 left join (
@@ -29,7 +30,7 @@ left join (
 	select b.`cod_art`, b.`descri`, SUM(b.`cantidad`)  as cantidad
 	from inv_bajas b
 	where b.`cod_alm` = 2
-	and b.`fecha` between '2019-01-01' and '2019-07-31'
+	and b.`fecha` between '2019-01-01' and '2019-08-17'
 	group by b.`cod_art`, b.`descri`
 	) ba on t.cod_art = ba.cod_art
 left join (
@@ -37,14 +38,14 @@ left join (
 	select b.`cod_art`, b.`descri`, SUM(b.`cantidad`)  as cantidad
 	from inv_reprocesos b
 	where b.`cod_alm` = 2
-	and b.`fecha` between '2019-01-01' and '2019-07-31'
+	and b.`fecha` between '2019-01-01' and '2019-08-17'
 	group by b.`cod_art`, b.`descri`
 	) br on t.cod_art = br.cod_art
 left join (
 	select d.`cod_art`, SUM(d.`cantidad`) as cantidad
 	from inv_movdet d
 	left join inv_vales v on d.`no_trans` = v.`no_trans`
-	where v.`fecha` between '2019-01-01' and '2019-07-31'
+	where v.`fecha` between '2019-01-01' and '2019-08-17'
 	and v.`cod_doc` in ('REC', 'EGR')
 	and v.`cod_alm` = 2
 	and v.`idordenproduccion` is null
@@ -57,7 +58,7 @@ left join (
 	select d.`cod_art`, SUM(d.`cantidad`) as cantidad
 	from inv_movdet d
 	left join inv_vales v on d.`no_trans` = v.`no_trans`
-	where v.`fecha` between '2019-01-01' and '2019-07-31'
+	where v.`fecha` between '2019-01-01' and '2019-08-17'
 	and v.`cod_doc` in ('REC', 'EGR')
 	and v.`cod_alm` = 2
 	and v.`idordenproduccion` is null
@@ -66,5 +67,7 @@ left join (
 	and v.estado <> 'ANL'
 	group by d.`cod_art`
 	) egr on t.cod_art = egr.cod_art
+left join inv_articulos a on t.cod_art = a.cod_art
 where t.alm = 2 and t.gestion = 2019
+and a.estado = 'VIG'
 ;
