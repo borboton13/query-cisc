6e079a4529f5747af618d7e3eb512372c2c6e733
AS <arielrse@gmail.com>
2019-11-28 13:19:45 -0400

    28.11.2019 1318

--- 2019/query_creditos.sql
+++ 2019/query_creditos.sql
@@ -16,8 +16,8 @@ where c.`idtipocredito` = 1
 ;
 
 -- ESTADO DE CARTERA VIGENTE
-select c.`idcredito`, s.`nombres`, s.`apellidopaterno`, s.`apellidomaterno`, c.`codigoant`, c.`estado`, c.`importe`, max(t.fechatransaccion) as fecha, 
-sum(t.`capital`) as pagado, c.`importe` - sum(t.`capital`) as saldo
+select c.`idcredito`, s.`nombres`, s.`apellidopaterno`, s.`apellidomaterno`, c.`codigoant`, c.`estado`, c.`importe`, MAX(t.fechatransaccion) as fecha, 
+SUM(t.`capital`) as pagado, c.`importe` - SUM(t.`capital`) as saldo
 from credito c
 join socio s 			on c.`idsocio` = s.`idsocio`
 join transaccioncredito t 	on c.`idcredito` = t.`idcredito`
@@ -26,7 +26,7 @@ and t.`fechatransaccion` <= '2019-01-31'
 group by c.`idcredito`, s.`nombres`, s.`apellidopaterno`, s.`apellidomaterno`, c.`codigoant`, c.`estado`, c.`importe`
 ;
 -- 
-select max(t.fechatransaccion) as fecha, sum(t.`capital`) as pagado, c.`importe` - sum(t.`capital`) as saldo
+select MAX(t.fechatransaccion) as fecha, SUM(t.`capital`) as pagado, c.`importe` - SUM(t.`capital`) as saldo
 from credito c
 join socio s 			on c.`idsocio` = s.`idsocio`
 join transaccioncredito t 	on c.`idcredito` = t.`idcredito`
@@ -38,7 +38,7 @@ and t.`fechatransaccion` <= '2019-01-31'
 
 
 select e.`fecha`, e.`tipo_doc`, e.`no_doc`, e.`glosa`, c.`idcredito`, c.`codigo`, c.`codigoant`, s.`nombres`, s.`apellidopaterno`, s.`apellidomaterno`, 
-concat(s.`nombres`,' ', s.`apellidopaterno`, ' ', s.`apellidomaterno`, ' ', c.`codigoant`, ', ', e.`glosa`)
+CONCAT(s.`nombres`,' ', s.`apellidopaterno`, ' ', s.`apellidomaterno`, ' ', c.`codigoant`, ', ', e.`glosa`)
 from sf_tmpenc e
 join sf_tmpdet d on e.`id_tmpenc` = d.`id_tmpenc`
 join credito c on d.`idcredito` = c.`idcredito`
@@ -64,6 +64,20 @@ set e.`fecha` = t.`fechatransaccion`
 where e.`fecha` <> t.`fechatransaccion`
 ;
 
+-- PARA ESTADO DE CARTERA
+select z.`nombre`, s.`nombres`, s.`apellidopaterno`, c.`codigoant`, c.estado, tc.`idmoneda`, c.`fechaconcesion`, c.`ultimopago`, c.`fechavence`, c.`importe`, SUM(t.`capital`) as capital, (c.`importe` - SUM(t.`capital`)) as saldo_capital,
+MAX(t.`fechatransaccion`) as ultima
+from transaccioncredito t
+left join credito c on t.`idcredito` = c.`idcredito`
+left join socio s on c.`idsocio` = s.`idsocio`
+left join zonaproductiva z on s.`idzonaproductiva` = z.`idzonaproductiva`
+left join tipocredito tc on c.`idtipocredito` = tc.`idtipocredito`
+where c.estado <> 'FIN'
+-- and z.`numero` = 15
+and tc.`idmoneda` = 2
+and t.`fechatransaccion` <= '2019-11-01'
+group by z.`nombre`, s.`nombres`, s.`apellidopaterno`, c.`codigoant`, c.`fechaconcesion`, c.`ultimopago`, c.`fechavence`, c.`importe`
+;
 
 
 
