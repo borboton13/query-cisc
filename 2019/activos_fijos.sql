ad924fa32c450f479523e34186a1f827944d53e0
Ariel S <ariel.siles@ilvabolivia.com>
2020-02-18 16:42:04 -0400

    18.02.2020 1618

--- 2019/activos_fijos.sql
+++ 2019/activos_fijos.sql
@@ -65,20 +65,47 @@ update af_activos a set a.`estado` = 'DEP' where a.`idactivo` = 364;
 update af_activos a set a.`dep_acu_vo` = 66604.65 where a.`idactivo` = 364;
 update af_activos a set a.`dep_acu_vo` = a.`vobs` where a.`estado` in ('BAJ', 'TDP');
 update af_activos a set a.`dep_vo` = round(a.`vobs`/a.`duracion`, 2) where a.`dep_vo` > 0 ;
-
 update af_activos a set a.`moneda` = 'P';
+--
+/** TEST Corregige acumulado al 31/12/2019 Edificios **/
+update af_activos set dep_acu_vo = 	148656.71	, dep_vo = 	1238.81	  where idactivo =  	23	 ; 
+update af_activos set dep_acu_vo = 	70840.89	, dep_vo = 	295.17	  where idactivo =  	27	 ; 
+update af_activos set dep_acu_vo = 	257177.60	, dep_vo = 	996.81	  where idactivo =  	36	 ; 
+update af_activos set dep_acu_vo = 	17411.10	, dep_vo = 	97.82	  where idactivo =  	145	 ; 
+update af_activos set dep_acu_vo = 	5294.89	, dep_vo = 	35.30	  where idactivo =  	182	 ; 
+update af_activos set dep_acu_vo = 	9625.00	, dep_vo = 	72.92	  where idactivo =  	208	 ; 
+update af_activos set dep_acu_vo = 	2461.96	, dep_vo = 	32.83	  where idactivo =  	319	 ; 
+
+
 
 --
 
-select a.`idactivo`, a.`descri`, a.`estado`, a.`vobs`, a.`voufv`, a.`dep_vo`, a.`duracion`, round(a.`vobs`/a.`duracion`, 2)
-from af_activos a 
-where a.`dep_vo` > 0
+select 	a.`idactivo`, a.`descri`, a.`fch_alta`, a.`vobs`, a.`voufv`, a.`vosus`, a.`tasa_dep`, a.`dep_vo`, a.`dep_acu_vo`, a.`estado`,
+	d.`idhdepre`, d.`fecha_en_proceso` as fecha , d.`tasabsufv`, d.`tasabssus`,
+	d.`dep_acu_vo`, d.`dep_acu_vo_bs`, d.`val_tot`,
+	d.`dep_vo`, d.`dep_vo_bs`
+from `af_activos` a
+left join af_hdepre d on a.`idactivo` = d.`idactivo`
+where a.`idactivo` in (23,27,36,145,182,208,319)
 ;
 
-select *
-from af_activos a
-where a.`estado` = 'DEP'
+select 	a.`idactivo`, a.`descri`, a.`fch_alta`, a.`duracion`, a.`vobs`, a.`dep_acu_vo`, a.`estado`, a.`dep_vo`,
+	sum(d.`dep_vo`) s_dep_vo, sum(d.`dep_vo_bs`) s_dep_vo_bs
+from `af_activos` a
+left join af_hdepre d on a.`idactivo` = d.`idactivo`
+where a.`idactivo` in (23,27,36,145,182,208,319)
+group by a.`idactivo`, a.`descri`, a.`fch_alta`, a.`duracion`, a.`vobs`, a.`dep_acu_vo`, a.`estado`, a.`dep_vo`
 ;
+
+
+
+
+
+
+
+
+
+
 
 
 
