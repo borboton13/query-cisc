2bbc5193d5f17104ecc53c9f1bc1dbcc77c53eb5
Ariel S <ariel.siles@ilvabolivia.com>
2020-11-03 15:53:24 -0400

    03112020 1553

--- 2019/query_rrhh.sql
+++ 2019/query_rrhh.sql
@@ -44,7 +44,7 @@ left join contrato c on cp.`idcontrato` = c.`idcontrato`
 left join empleado e on c.`idempleado` = e.`idempleado`
 left join persona p  on e.`idempleado` = p.`idpersona`
 left join entidad en on p.`idpersona` = en.`identidad`
-where en.`noidentificacion` = 9458393
+where en.`noidentificacion` = 5197188
 and bc.`fechafin` >= '2020-01-01'
 -- and bh.`horainicio` = '08:00:00'
 ;
@@ -57,9 +57,10 @@ left join contrato c on cp.`idcontrato` = c.`idcontrato`
 left join empleado e on c.`idempleado` = e.`idempleado`
 left join persona p  on e.`idempleado` = p.`idpersona`
 left join entidad en on p.`idpersona` = en.`identidad`
-set bh.`horainicio` = '12:00', bh.horafin = '20:00'
+-- set bh.`horainicio` = '12:00', bh.horafin = '20:00'
 -- set bh.horafin = '18:00:00'
-where en.`noidentificacion` = 6405572
+set bc.fechafin = '2020-10-03'
+where en.`noidentificacion` = 5197188
 and bc.`fechafin` >= '2020-01-01'
 -- and bh.`horainicio` = '07:00:00'
 -- and bh.idbandahoraria in (1028)
@@ -84,9 +85,16 @@ where idbandahoraria in (
 );
 
 -- eliminando planillas generadas
-delete from reportecontrol 		 	where idplanillagenerada in (406, 407);
-delete from `planillafiscalporcategoria` 	where idplanillagenerada in (406, 407);
-delete from `planillatributariaporcategoria` 	where idplanillagenerada in (406, 407);
-delete from `planillaadministrativos` 		where idplanillagenerada in (406, 407);
-delete from `planillagenerada` 			where idplanillagenerada in (406, 407);
+delete from reportecontrol 		 	where idplanillagenerada in (412, 413, 414, 415, 416, 417);
+delete from `planillafiscalporcategoria` 	where idplanillagenerada in (412, 413, 414, 415, 416, 417);
+delete from `planillatributariaporcategoria` 	where idplanillagenerada in (412, 413, 414, 415, 416, 417);
+delete from `planillaadministrativos` 		where idplanillagenerada in (412, 413, 414, 415, 416, 417);
+delete from `planillagenerada` 			where idplanillagenerada in (412, 413, 414, 415, 416, 417);
+
+
+update planillafiscalporcategoria p
+set 	p.`liquidopagable` = 14137.84,
+	p.`afplab_riesgocomun` = 0,
+	p.`totaldescuentos` = 1780.16
+where p.`idplanillafiscalporcategoria` =       17150;
 
