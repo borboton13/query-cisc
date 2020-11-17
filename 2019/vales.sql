a67304b5bb05b6cebc3890c11d402751ce4f9369
Ariel S <ariel.siles@ilvabolivia.com>
2020-11-17 15:58:24 -0400

    17112020 1558

--- 2019/vales.sql
+++ 2019/vales.sql
@@ -1,16 +1,28 @@
 -- EGRESOS
 select *
 from inv_vales i
-where i.`fecha` between '2020-04-01' and '2020-12-31'
+where i.`fecha` between '2020-10-01' and '2020-10-31'
 and i.`cod_doc` = 'EGR'
 and i.`oper` is null
-and i.`cod_alm` in (1,3,4,7)
+and i.`estado` <> 'ANL'
+-- and i.`cod_alm` in (1,3,4,7)
 ;
+
+
+select *
+from inv_vales i
+where i.`no_vale` = '3-4493'
+;
+
+-- Baja de bobina
+update inv_vales i set i.`estado` = 'ANL' where I.`no_trans` = 25100;
+update inv_mov i set i.`estado` = 'ANL' where I.`no_trans` = 25100;
+update inv_movdet i set i.`estado` = 'ANL' where I.`no_trans` = 25100;
 
 -- RECEPCION
 select *
 from inv_vales i
-where i.`fecha` between '2020-03-01' and '2020-12-31'
+where i.`fecha` between '2020-09-01' and '2020-09-31'
 and i.`cod_doc` = 'REC'
 and i.`oper` is not null
 -- and i.`cod_alm` in (1,3)
@@ -21,10 +33,10 @@ select i.`fecha`, i.`no_vale`, i.`cod_doc`, i.`estado`, d.`cod_art`, a.`descri`,
 from inv_vales i
 join inv_movdet d on i.`no_trans` = d.`no_trans`
 join inv_articulos a on d.`cod_art` = a.`cod_art`
-where i.`fecha` between '2020-01-01' and '2020-01-31'
+where i.`fecha` between '2020-09-01' and '2020-09-31'
 and i.`cod_doc` = 'EGR'
 and i.`oper` is null
-and i.`cod_alm` in (1)
+-- and i.`cod_alm` in (1)
 ;
 
 
